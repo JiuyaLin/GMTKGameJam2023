4b26dc0208cbc6f9b0ebb0bfa31562f4515cc2df
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-07-08 13:47:20 -0400

    Update UserInput.cs

--- GMTK Jam/Assets/Scripts/GameHandler/UserInput.cs
+++ GMTK Jam/Assets/Scripts/GameHandler/UserInput.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class UserInput : MonoBehaviour
 {
     
-    public AttackMake playerAttackMake;
+    public GameObject player;
     // Update is called once per frame
+    private AttackMake playerAttackMake;
+
+    void Start() {
+        playerAttackMake = player.GetComponentInChildren<AttackMake>()[0];
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) {
