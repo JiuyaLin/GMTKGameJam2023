0cbb628ef8bceb867586ff6761050ed73d64708c
Alexander Koppel <alexk@thekoppels.net>
2023-07-08 18:47:01 -0400

    Projectile Bug Fix

--- GMTK Jam/Assets/Scripts/AttackScripts/Damage/RangeDamage.cs
+++ GMTK Jam/Assets/Scripts/AttackScripts/Damage/RangeDamage.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 public class RangeDamage : MonoBehaviour
 {
     public int damage = 1;
+    public float lifetime = 1;
+    public float speed = 10;
     public bool isPlayer = false;
+    public bool canBounce = false;
+
+    void Start()
+    {
+        transform.parent = null;
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime < 0) Destroy(gameObject);
+    }
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider collision) {
@@ -14,7 +30,7 @@ public class RangeDamage : MonoBehaviour
             item.OnRangeHit(collision.gameObject);
         }
 
-        if (isPlayer){
+        if (isPlayer) {
             if (collision.tag != "Enemy") return;
             int totalDamage = PlayerStats.rangeDamage + damage;
             totalDamage = totalDamage > 0 ? totalDamage : 0;
@@ -22,6 +38,13 @@ public class RangeDamage : MonoBehaviour
         } else {
             if (collision.tag != "Player") return;
             PlayerStats.hp -= damage;
+        }
+
+        if (canBounce) {
+            lifetime *= 2;
+            GetComponent<Rigidbody>().velocity *= 1.5f;
+        } else {
+            Destroy(gameObject);
         }
     }
 }
