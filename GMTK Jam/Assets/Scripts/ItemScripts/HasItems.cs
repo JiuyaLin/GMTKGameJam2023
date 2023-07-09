46727f7e25a46ae40a2836af1da71e573a5beb49
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-07-09 02:58:44 -0400

    Gold Coin fix

--- GMTK Jam/Assets/Scripts/ItemScripts/HasItems.cs
+++ GMTK Jam/Assets/Scripts/ItemScripts/HasItems.cs
@@ -9,15 +9,43 @@ using UnityEngine;
 public class HasItems : MonoBehaviour
 {
     public static bool droppedReactorCore = false;
-    public static bool droppedLockpick = true; // assumes you don't have it to start with
     public static bool droppedWingedShoes = true;
-    
-
+    public static bool droppedGold = false;
+    public static bool droppedLockpick = true;
     // Assumes you HAVEN'T dropped anything at the beginning
     void Awake()
     {
-        droppedReactorCore = false;
-        droppedLockpick = true;
-        droppedWingedShoes = true;
+        stateReset();
+        
     }   
+
+    public void stateReset() {
+        droppedReactorCore = false;
+        droppedWingedShoes = true;
+        droppedGold = false;
+        droppedLockpick = true;
+        foreach (Item item in ItemList.itemList) {
+            if (item.GetName() == "Winged Shoes") 
+                droppedWingedShoes = false;
+            if (item.GetName() == "Lockpick") 
+                droppedWingedShoes = false;
+        }
+    }
+
+    static float timer = 0;
+    static float duration = 0;
+    public static void dropGold(float waitTime) {
+        timer = Time.time;
+        duration = waitTime;
+        droppedGold = true;
+    }
+    // For the continuous effects ie the gold
+    void Update() {
+        Debug.Log("Hi");
+        if (droppedGold) {
+            if (Time.time - timer > duration) 
+                PlayerStats.hp = 0;
+            Debug.Log("Sad");
+        }
+    }
 }
