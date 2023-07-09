7f82b681773aeb1286bdb37ca0f13d53568da64d
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-07-09 11:04:19 -0400

    Update ButtonDescription.cs

--- GMTK Jam/Assets/Scripts/UI/ButtonDescription.cs
+++ GMTK Jam/Assets/Scripts/UI/ButtonDescription.cs
@@ -9,16 +9,18 @@ public class ButtonDescription : MonoBehaviour
     public GameObject descriptionBox; 
     public TMP_Text text;
 
+    Vector2 screen;
     RectTransform tf;
     void Start() {
         tf = descriptionBox.GetComponent<RectTransform>();
+        screen = new Vector2(Screen.width, Screen.height);
     }
     public void openDisplay(int itemIndex) {
         
         descriptionBox.SetActive(true);
         Vector2 mousePosition = Input.mousePosition;
         mousePosition.y -= tf.rect.height / 2;
-        
+        mousePosition = Vector2.Scale(mousePosition.normalized, screen);
         tf.anchoredPosition = mousePosition;
         text.text = ItemList.itemList[itemIndex].GetDescription();
         // Debug.Log("This is good");
