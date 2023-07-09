55f92bbb8a9bedc6a02148e19633fdc41d054cbe
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-07-09 12:08:14 -0400

    button desc

--- GMTK Jam/Assets/Scripts/UI/ButtonDescription.cs
+++ GMTK Jam/Assets/Scripts/UI/ButtonDescription.cs
@@ -11,17 +11,33 @@ public class ButtonDescription : MonoBehaviour
 
     Vector2 screen;
     RectTransform tf;
+    Vector2 offset;
+    Canvas canvas;
     void Start() {
         tf = descriptionBox.GetComponent<RectTransform>();
         screen = new Vector2(Screen.width, Screen.height);
+        canvas = transform.parent.parent.GetComponent<Canvas>();
+        offset = new Vector2(0, -1.5f * tf.rect.height);
+
     }
     public void openDisplay(int itemIndex) {
         
         descriptionBox.SetActive(true);
-        Vector2 mousePosition = Input.mousePosition;
-        mousePosition.y -= tf.rect.height / 2;
-        mousePosition = Vector2.Scale(mousePosition.normalized, screen);
-        tf.anchoredPosition = mousePosition;
+        // Vector2 mousePosition = Input.mousePosition;
+        // Vector2 pos;
+        // RectTransformUtility.ScreenPointToLocalPointInRectangle(
+        //     canvas.transform as RectTransform,
+        //     mousePosition,
+        //     canvas.worldCamera,
+        //     out pos
+        // );
+        // pos += offset;
+        
+        // mousePosition = mousePosition / canvas.scaleFactor;
+        
+        // Debug.Log(Input.mousePosition + " " + mousePosition);
+        
+        //tf.anchoredPosition = pos;
         text.text = ItemList.itemList[itemIndex].GetDescription();
         // Debug.Log("This is good");
         Debug.Log(ItemList.itemList[itemIndex].GetName());
